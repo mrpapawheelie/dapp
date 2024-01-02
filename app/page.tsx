b14c2485316dfe568b257020c98670462ba76c0f
mrpapawheelie <papa@cooks.dev>
2024-01-02 13:59:38 -0700

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+'use client'
+import "./globals.css";
+import { useAccount } from "wagmi";
+
+export default function Home() {
+  const { address, isConnecting, isDisconnected } = useAccount();
+  if (isConnecting) return <div>Connecting...</div>;
+  if (isDisconnected) return <div>Disconnected</div>;
+  return <div>Connected Wallet: {address}</div>;
+};
---
+++ components/instructionsComponent/navigation/Wallet.tsx
@@ -0,0 +1,7 @@
+import { ConnectKitButton } from "connectkit";
+
+export function Wallet() {
+  return (
+    <ConnectKitButton />
+  );
+}
